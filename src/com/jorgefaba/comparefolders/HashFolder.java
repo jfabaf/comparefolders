3dd86a63f7eb500605838f2f24431adf2e67d302
Jorge Fabá Ferrández <jfabaf@gmail.com>
2014-09-08 16:50:12 +0200

    Apache commons cli arg parser, save hastable to file, progress

--- src/com/jorgefaba/comparefolders/HashFolder.java
+++ src/com/jorgefaba/comparefolders/HashFolder.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Hashtable;
+
+import org.apache.commons.io.FileUtils;
 
 public class HashFolder implements Serializable {
 	
@@ -16,22 +19,44 @@ public class HashFolder implements Serializable {
 	private static final long serialVersionUID = -4098355192873388828L;
 	Hashtable<String,FileHash>	hash = new Hashtable<String,FileHash>();
 	HashSet<String> processedPaths = new HashSet<String>();
-	File	folder;
-	
+	File			folder;
+	boolean			progress;
+	BigInteger 		sizeFolder;
+	BigInteger		sizeProcessed = BigInteger.ZERO;
+	int				percentajeDone;
 
 	public HashFolder(String folderstr) throws Exception {
+		this(folderstr,false);
+	}
+	
+	public HashFolder(String folderstr, boolean progress) throws Exception {
 		super();
 		folder = new File(folderstr);
 		if (!folder.isDirectory())
 			throw new Exception("Folder " + folderstr + " is not a folder");
+		this.progress = progress;
+		if (progress) {
+			System.out.printf("Processing folder '%s':\n",folder.getAbsolutePath());
+			sizeFolder = FileUtils.sizeOfDirectoryAsBigInteger(folder);
+		}
 		generateHash(folder);
 		
 	}
 	
 	private void generateHash(File file) throws IOException {
+		int				percentajeDoneAux;
 		if (file.isFile()) {
 			String md5 = getMD5(file);
 			hash.put(md5, new FileHash(file.getPath(),md5));
+			if (progress) {
+				sizeProcessed = sizeProcessed.add(FileUtils.sizeOfAsBigInteger(file));
+				percentajeDoneAux =  sizeProcessed.divide(sizeFolder).multiply(BigInteger.valueOf(100)).intValue();
+				if (percentajeDoneAux > percentajeDone) {
+					percentajeDone = percentajeDoneAux;
+					System.out.printf("%d%% done\n",percentajeDone);
+				}
+				
+			}
 		} else if (!processedPaths.contains(file.getCanonicalPath())){
 			processedPaths.add(file.getCanonicalPath());
 			File files[] = file.listFiles();
@@ -56,6 +81,11 @@ public class HashFolder implements Serializable {
 	
 	public String getProcessedPaths() {
 		return processedPaths.toString();
+	}
+	
+	
+	public String getFolderPath() {
+		return folder.getAbsolutePath();
 	}
 	
 	
--- src/com/jorgefaba/comparefolders/Main.java
+++ src/com/jorgefaba/comparefolders/Main.java
@@ -1,6 +1,16 @@
 package com.jorgefaba.comparefolders;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
 import org.apache.commons.cli.*;
 
@@ -16,26 +26,19 @@ public class Main {
 	private static final Options options = new Options();
 	private static CommandLine cml;
 	public static void main(String[] args) {
-		String folder1;
-		String folder2;
 		HashFolder hashFolder1;
 		HashFolder hashFolder2;
-		String[] folders;
 		cml = parseArgs(args);
-		folders = cml.getArgs();
-		
-		//get folder1
-		
-		
-		
-		checkArgs(args);
-		folder1 = args[0];
-		folder2 = args[1]; 
+		if (!checkArgs()) {
+			usage();
+			System.exit(-1);
+		}
+
 		try {
-			hashFolder1 = new HashFolder(folder1);
-			hashFolder2 = new HashFolder(folder2);
-			System.out.println("folder1: " + folder1);
-			System.out.println("folder2: " + folder2);
+			hashFolder1 = getHashFolder(1);
+			hashFolder2 = getHashFolder(2);
+			System.out.println("folder1: " + hashFolder1.getFolderPath());
+			System.out.println("folder2: " + hashFolder2.getFolderPath());
 			System.out.println("folder1:processedpaths " + hashFolder1.getProcessedPaths());
 			System.out.println("folder2:processedpaths " + hashFolder2.getProcessedPaths());
 			for (FileHash file: hashFolder1.getFiles()) {
@@ -49,31 +52,81 @@ public class Main {
 					System.out.println("File '" + file.getPath() + "' not found in folder1");
 				}
 			}
+			saveHashFolders(hashFolder1, hashFolder1);
 		} catch (Exception e) {
 			System.out.println("An error is ocurred: " + e.toString());
 		}
 	}
 	
-	private static void checkArgs(String[] args) {
-		if (args.length != 2) {
-			usage();
-			System.exit(-1);
+	private static void saveHashFolders(HashFolder ht1, HashFolder ht2) {
+		if (cml.hasOption("hs1")) {
+			saveHashFolderToFile(cml.getOptionValue("hs1"), ht1);
 		}
+		if (cml.hasOption("hs2")) {
+			saveHashFolderToFile(cml.getOptionValue("hs2"), ht2);
+		}
+	}
+	
+	private static void saveHashFolderToFile(String pathToFile, HashFolder ht) {
+		//serialize
+	    try (
+	      OutputStream file = new FileOutputStream(pathToFile);
+	      OutputStream buffer = new BufferedOutputStream(file);
+	      ObjectOutput output = new ObjectOutputStream(buffer);
+	    ){
+	      output.writeObject(ht);
+	    }  
+	    catch(IOException e){
+	    	System.out.println("An error is ocurred while writing Hashfolder (" + pathToFile + ") : " + e.toString());
+	    }
+	}
+	
+	private static boolean checkArgs() {
+		boolean dev = true;
+		
+		//folder1
+		if (cml.hasOption("f1") && cml.hasOption("ht1")) {
+			dev = false;
+			System.err.println("Options f1 and ht1 couldn't be together");
+		}
+		
+		if (cml.hasOption("ht1") && cml.hasOption("hs1")) {
+			dev = false;
+			System.err.println("Options ht1 and hs1 couldn't be together");
+		}
+		
+		if (!cml.hasOption("f1") && !cml.hasOption("ht1")) {
+			dev = false;
+			System.err.println("You need f1 or ht1 option (one of them)");
+		}
+				
+		//folder2
+		if (cml.hasOption("f2") && cml.hasOption("ht2")) {
+			dev = false;
+			System.err.println("Options f2 and ht2 couldn't be together");
+		}
+		
+		if (cml.hasOption("ht2") && cml.hasOption("hs2")) {
+			dev = false;
+			System.err.println("Options ht2 and hs2 couldn't be together");
+		}
+		
+		if (!cml.hasOption("f2") && !cml.hasOption("ht2")) {
+			dev = false;
+			System.err.println("You need f2 or ht2 option (one of them)");
+		}
+
+		return dev;
+			
 	}
 	
 	private static CommandLine parseArgs(String[] args) {
 		
-		//save hashtable1
-		Option hashtable1save = new Option("hs1","hashtablesave1", true,
-	      "save hashtable of folder1 to file");
-		hashtable1save.setArgName("file");
-		options.addOption(hashtable1save);
-
-		//save hashtable2
-		Option hashtable2save = new Option("hs2","hashtablesave2", true,
-			      "save hashtable of folder2 to file");
-		hashtable2save.setArgName("file");
-		options.addOption(hashtable2save);
+		//folder1
+		Option folder1 = new Option("f1","folder1", true,
+	      "Path to folder 1");
+		folder1.setArgName("folder1");
+		options.addOption(folder1);
 		
 		//read hashtable1
 		Option hashtable1 = new Option("ht1","hashtable1", true,
@@ -81,11 +134,35 @@ public class Main {
 		hashtable1.setArgName("file");
 		options.addOption(hashtable1);
 
-		//read hashtable1
+		//save hashtable1
+		Option hashtable1save = new Option("hs1","hashtablesave1", true,
+	      "save hashtable of folder1 to file");
+		hashtable1save.setArgName("file");
+		options.addOption(hashtable1save);
+
+		//folder2
+		Option folder2 = new Option("f2","folder2", true,
+	      "Path to folder 2");
+		folder2.setArgName("folder2");
+		options.addOption(folder2);	
+		
+		//read hashtable2
 		Option hashtable2 = new Option("ht2","hashtable2", true,
-			      "Use hashtable2 file as a folder1");
-		hashtable1.setArgName("file");
+			      "Use hashtable2 file as a folder2");
+		hashtable2.setArgName("file");
 		options.addOption(hashtable2);
+
+		//save hashtable2
+		Option hashtable2save = new Option("hs2","hashtablesave2", true,
+			      "save hashtable of folder2 to file");
+		hashtable2save.setArgName("file");
+		options.addOption(hashtable2save);
+
+		//progress
+		Option progress = new Option("sp","showprogress", false,
+			      "Show percentaje of progress");
+		options.addOption(progress);		
+
 		// create the parser
 	    CommandLineParser parser = new PosixParser();
 	    CommandLine line = null;
@@ -108,9 +185,52 @@ public class Main {
         formatter.printHelp(NAME + " ", options, true);
 	}
 	
-	private static HashFolder getHashFolder(String filebin, String pathfolder) {
+	private static HashFolder getHashFolder(int nfolder) {
+		HashFolder hf = null;
+		String htPath = null;
+		try {
+			if (cml.hasOption("f" + Integer.toString(nfolder))) {
+				if (!cml.hasOption("sp"))
+					hf = new HashFolder(cml.getOptionValue("f" + Integer.toString(nfolder)));
+				else
+					hf = new HashFolder(cml.getOptionValue("f" + Integer.toString(nfolder)),true);
+			}
+
+			if (cml.hasOption("ht" + Integer.toString(nfolder))) {
+				htPath = cml.getOptionValue("ht" + Integer.toString(nfolder));
+				hf = readHashFolderFromFile(htPath);
+			}
+			
+			
+		} catch (Exception e) {
+			System.err.println("An error is ocurred: " + e.toString());
+			System.exit(-1);
+		}
+		 
+		return hf;
 		
-		
+	}
+	
+	private static HashFolder readHashFolderFromFile(String pathToFile) {
+		HashFolder hf = null;
+		//deserialize
+	    try(
+	      InputStream file = new FileInputStream(pathToFile);
+	      InputStream buffer = new BufferedInputStream(file);
+	      ObjectInput input = new ObjectInputStream (buffer);
+	    ){
+	      //deserialize
+	      hf = (HashFolder)input.readObject();
+	    }
+	    catch(ClassNotFoundException e){
+	    	System.err.println("An error is ocurred: " + e.toString());
+			System.exit(-1);
+	    }
+	    catch(IOException e){
+	    	System.err.println("An error is ocurred: " + e.toString());
+			System.exit(-1);
+	    }		
+		return hf;
 	}
 	
 
